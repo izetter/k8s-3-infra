473844bdd9fba9660b070e817ea0f18f9dc54f2b
izetter <59151193+izetter@users.noreply.github.com>
2024-04-11 15:46:33 -0600

    k8s trial 3 initial commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# Top level infrastructure (VPC, Subnets, Security Groups, etc)
+
+variable "region" {
+	description = "AWS region"
+	type        = string
+	default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+	description = "CIDR block for the VPC"
+	type        = string
+	default     = "13.0.0.0/16"
+}

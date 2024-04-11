e9466be12dd5730a9c93de6e78b9e77d32fb5af1
izetter <59151193+izetter@users.noreply.github.com>
2024-04-11 17:15:14 -0600

    igw

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_vpc" "eks_vpc" {
     Name = "EKSVPC-3"
   }
 }
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "vpc_id" {
   value       = aws_vpc.eks_vpc.id
   description = "VPC id"
 }
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.eks_igw.id
+}

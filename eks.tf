2c7ce5992291df02ad18e9290841ddce06980851
izetter <59151193+izetter@users.noreply.github.com>
2024-04-11 22:58:49 -0600

    Added policies and role for EKS node group

--- eks.tf
+++ eks.tf
@@ -1,5 +1,4 @@
-# EKS Roles and Policies ===============================================================
-
+# EKS Cluster - Roles and Policies ========================================================
 
 # Role that EKS will assume to create AWS resources for Kubernetes clusters.
 resource "aws_iam_role" "eks_cluster_role" {
@@ -18,13 +17,13 @@ resource "aws_iam_role" "eks_cluster_role" {
 }
 
 # Permissions so K8S control plane can manage pertinent AWS resources via AWS API
-resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
 
 
-# EKS Cluster =========================================================================
+# EKS Cluster ============================================================================
 
 resource "aws_eks_cluster" "eks" {
   name     = "eks-3"
@@ -42,5 +41,39 @@ resource "aws_eks_cluster" "eks" {
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.amazon_eks_cluster_policy]
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
+
+
+# EKS Node Group - Roles and Policies ====================================================
+
+resource "aws_iam_role" "eks_nodes_role" {
+  name = "eks-node-group-general"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+

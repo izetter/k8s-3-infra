cad1d7b9d61149ac39b4d4f059eb84df43f79135
izetter <59151193+izetter@users.noreply.github.com>
2024-04-12 01:20:58 -0600

    Outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "vpc_id" {
 output "internet_gateway_id" {
   value = aws_internet_gateway.eks_igw.id
 }
+
+output "node_group_id" {
+  value = aws_eks_node_group.node_group.id
+}
+
+output "aws_eks_cluster_id" {
+  value = aws_eks_cluster.eks.id
+}
